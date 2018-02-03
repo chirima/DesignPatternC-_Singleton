4838a3eb3fb150891fa20591f6894593c2ebb487
ABHISHEK-PC\Abhi <abhishek_chr@hotmail.com>
2018-02-04 03:20:57 +1100

    + Define Observer Interface.

--- ObserverPattern/ObserverPattern/ObserverPattern.cpp
+++ ObserverPattern/ObserverPattern/ObserverPattern.cpp
@@ -2,7 +2,42 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include "Observer.h"
 
+using namespace std;
+
+class Person
+{
+private:
+	int age;
+public:
+	int get_age() const
+	{
+		return age;
+	}
+
+	void set_age(int age)
+	{
+		Person::age = age;
+	}
+};
+
+struct ConsolePersonObserver : public Observer<Person>
+{
+private:
+	void field_changed(Person& source, const std::string& field_name) override
+	{
+		cout << "Person's" << field_name << "has changed to ";
+		if (field_name == "age")
+		{
+			cout << source.get_age();
+		}
+		cout << "\n.";
+	}
+};
 
 int main()
 {
